cd418fa46ba3c0cdae98c114a80581dea1d08696
Brandon Hinkle <lewishinkle2020@gmail.com>
2025-01-26 13:04:19 -0500

    Change to Happiness System

--- data/events/happiness_changes.asm
+++ data/events/happiness_changes.asm
@@ -2,10 +2,10 @@ HappinessChanges:
 ; entries correspond to HAPPINESS_* constants
 	table_width 3, HappinessChanges
 	; change if happiness < 100, change if happiness < 200, change otherwise
-	db  +10, +6,  +4 ; Gained a level
+	db  +8,  +4,  +2 ; Gained a level
 	db  +5,  +3,  +2 ; Vitamin
 	db  +1,  +1,  +1 ; X Item
-	db  +9,  +9,  +9 ; Battled a Gym Leader
+	db  +7,  +7,  +7 ; Battled a Gym Leader
 	db  +1,  +1,  +1 ; Learned a move
 	db  -1,  -1,  -1 ; Lost to an enemy
 	db  -5,  -5, -10 ; Fainted due to poison
